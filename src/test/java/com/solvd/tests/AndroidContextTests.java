fe7c60d90d3de73677a963c9d5a972fa38ac46c8
Tina Kakhniashvili <tinakakhniashvili@MacBookAir.netis.cc>
2026-03-06 10:28:16 +0400

    Resolve comemnts from prev branch

--- src/test/java/com/solvd/tests/AndroidContextTests.java
+++ src/test/java/com/solvd/tests/AndroidContextTests.java
@@ -1,12 +1,11 @@
 package com.solvd.tests;
 
-import com.solvd.pages.android.HomePage;
+import com.solvd.pages.common.HomePageBase;
+import com.solvd.utils.MobileContextUtils;
 import com.zebrunner.carina.core.AbstractTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
-import java.util.Set;
 
 public class AndroidContextTests extends AbstractTest {
 
@@ -18,22 +17,20 @@ public class AndroidContextTests extends AbstractTest {
     }
 
     @Test
-    public void verifyContextSwitchingInAndroidChrome() {
-        HomePage homePage = initPage(getDriver(), HomePage.class);
+    public void verifyContextSwitching() {
+        MobileContextUtils contextUtils = new MobileContextUtils();
+
+        HomePageBase homePage = initPage(getDriver(), HomePageBase.class);
         homePage.open();
-
-        Set<String> contexts = homePage.getAvailableContexts();
-        softly.assertTrue(contexts.contains("NATIVE_APP"), "NATIVE_APP context should exist.");
-        softly.assertTrue(contexts.size() > 1, "Web context should exist in addition to native context.");
-
         homePage.triggerNativeRequiredActionInWeb();
 
-        homePage.handleNativePopup();
-        softly.assertEquals(homePage.getCurrentContext(), "NATIVE_APP", "Context should be NATIVE_APP after switching.");
+        contextUtils.switchMobileContext(MobileContextUtils.View.NATIVE);
+        getDriver().navigate().back();
+        contextUtils.switchMobileContext(MobileContextUtils.View.WEB_BROWSER);
 
-        homePage.switchBackToWeb();
-        softly.assertNotEquals(homePage.getCurrentContext(), "NATIVE_APP", "Context should switch back to web context.");
-
+        homePage.open();
+        softly.assertTrue(getDriver().getCurrentUrl().contains("prestashop.com"),
+                "Unexpected URL after switching back to WEB context: " + getDriver().getCurrentUrl());
         softly.assertAll();
     }
 }
